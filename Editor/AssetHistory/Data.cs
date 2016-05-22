ed72439fd7bfd7a4d556c3b1e04c25c8281b1a0f
Kitahara Hiroki <h_kitahara@cyberconnect.local>
2016-05-22 14:47:01 +0900

    ソース追加

---
+++ Editor/AssetHistory/Data.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using System.Collections.Generic;
+using UnityEditor;
+
+namespace AssetHistory
+{
+	public class Data : ScriptableObject
+	{
+		public List<string> histries = new List<string>();
+
+		public List<AccessCount> accessCounts = new List<AccessCount>();
+
+		public Mode mode;
+	}
+
+	public enum Mode : int
+	{
+		History,
+		AccessCount,
+	}
+
+	[System.Serializable]
+	public class AccessCount
+	{
+		public string path;
+
+		public int accessCount;
+
+		public AccessCount(string path)
+		{
+			this.path = path;
+			this.accessCount = 1;
+		}
+	}
+}
