79dfc031b1a34675cb91efe236706921b34eadf3
Kitahara Hiroki <h_kitahara@cyberconnect.local>
2016-05-25 15:50:58 +0900

    プロジェクト追加
    フィルター機能を追加

--- Assets/Editor/AssetHistory/Data.cs
+++ Assets/Editor/AssetHistory/Data.cs
@@ -7,9 +7,11 @@ namespace AssetHistory
 {
 	public class Data : ScriptableObject
 	{
-		public List<string> histries = new List<string>();
+		public List<string> guids = new List<string>();
 
 		public List<AccessCount> accessCounts = new List<AccessCount>();
+
+        public List<Filter> filters = new List<Filter>();
 
 		public Mode mode;
 	}
@@ -33,4 +35,18 @@ namespace AssetHistory
 			this.accessCount = 1;
 		}
 	}
+
+    [System.Serializable]
+    public class Filter
+    {
+        public string name;
+
+        public bool valid;
+
+        public Filter(string name)
+        {
+            this.name = name;
+            this.valid = true;
+        }
+    }
 }
